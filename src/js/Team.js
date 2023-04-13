e7609257a3efe65edc8e85ffaef0ad9d7c78c211
Анастасия Ельшина <salnaya92@gmail.com>
2023-04-13 13:48:56 +0300

    Teams.js was edited

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,6 +1,37 @@
 export default class Team {
-  constructor(team) {
-    this.team = [team];
-    [Symbol.iterator] = [][Symbol.iterator];
+  constructor() {
+    this.team = new Set();
+  }
+
+  [Symbol.iterator]() {
+    const team = Object.entries(this.team);
+    let index = 0;
+    return {
+      next() {
+        if (team.length > 5) {
+          return {
+            value: undefined,
+            done: true,
+          };
+        }
+        index += 1;
+        return {
+          value: team[index],
+          done: false,
+        };
+      },
+    };
+  }
+
+  add(member) {
+    if (this.team.has(member)) {
+      throw new Error('Такой персонаж уже есть в команде!');
+    }
+    return this.team.add(member);
+  }
+
+  addAll(...members) {
+    members.forEach((member) => this.team.add(member));
+    return this.team;
   }
 }
